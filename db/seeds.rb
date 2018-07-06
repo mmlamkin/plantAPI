2be02a81ee70a69e3ca1960b3a3b6fab43255b27
Mary Lamkin <mmlamkin@gmail.com>
2018-07-06 10:26:09 -0700

    adds zone 3 planting dates web scraping code for list of veggies

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,38 @@ require 'open-uri'
 # end
 #
 # puts all_plants_array
+
+baseURL = "http://www.thevegetablegarden.info/planting-schedules"
+
+calendarPage = Nokogiri::HTML(open("http://www.thevegetablegarden.info/resources/planting-schedules/zones-7-8-planting-schedule"))
+
+
+veg_name_array = []
+veg_spring_dates = []
+veg_fall_dates = []
+
+calendarPage.css('table tr td:first-child').each do |veg|
+  veg_name_array << veg.css('span').inner_text
+end
+
+calendarPage.css('table tr td:nth-child(3)').each do |veg|
+  veg_spring_dates << veg.css('span').inner_text
+end
+
+calendarPage.css('table tr td:nth-child(4)').each do |veg|
+  veg_fall_dates << veg.css('span').inner_text
+end
+
+zone_3_array = []
+
+i=0
+
+while i < veg_name_array.length
+  zone_3_array << { name: veg_name_array[i],
+  spring_dates: veg_spring_dates[i],
+  fall_dates: veg_fall_dates[i]
+}
+  i += 1
+end
+
+puts zone_3_array

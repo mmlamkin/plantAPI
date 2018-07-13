88494f2c4ddd1102b9ecd14965e6a37d0ff254eb
Mary Lamkin <mmlamkin@gmail.com>
2018-07-13 14:23:23 -0700

    uncommented lines in join table migration

--- db/migrate/20180708222120_create_join_table_gardens_plants.rb
+++ db/migrate/20180708222120_create_join_table_gardens_plants.rb
@@ -1,8 +1,8 @@
 class CreateGardensPlants < ActiveRecord::Migration[5.1]
   def change
     create_join_table :gardens, :plants do |t|
-      # t.index [:garden_id, :plant_id]
-      # t.index [:plant_id, :garden_id]
+      t.index [:garden_id, :plant_id]
+      t.index [:plant_id, :garden_id]
     end
   end
 end

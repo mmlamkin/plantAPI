19409593740182ef64d3bc692482009292c0a2c2
Mary Lamkin <mmlamkin@gmail.com>
2018-07-19 18:05:40 -0700

    takes cookies/checkpending out of app.rb

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,6 @@ module PlantAPI
       end
     end
 
-    config.middleware.insert_after ActiveRecord::Migration::CheckPending, ActionDispatch::Cookies
-   config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
